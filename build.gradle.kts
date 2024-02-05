5c2bf085b1f7d6fdc84926f6f614899bd68bd899
david steinsland <david@davidsteinsland.net>
2024-02-05 08:09:10 +0100

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:2024020422121707081126.d834a5d05bb2")
+    implementation("com.github.navikt:rapids-and-rivers:2024020507581707116327.1c34df474331")
     implementation("org.postgresql:postgresql:42.6.0")
     implementation("com.github.seratch:kotliquery:1.9.0")
     implementation("com.zaxxer:HikariCP:5.0.1")
