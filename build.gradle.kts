9cdfa5b950dbaabb65924038465ba1ab22615ba2
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2024-02-21 09:59:30 +0100

    Bump postgresql to 42.7.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 
 dependencies {
     implementation("com.github.navikt:rapids-and-rivers:2024020507581707116327.1c34df474331")
-    implementation("org.postgresql:postgresql:42.6.0")
+    implementation("org.postgresql:postgresql:42.7.2")
     implementation("com.github.seratch:kotliquery:1.9.0")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("org.flywaydb:flyway-core:9.17.0")
